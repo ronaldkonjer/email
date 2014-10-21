54e2f84246e5202b3b801bc9dcf8fa5992fdf7f3
ronaldkonjer <ronaldkonjer@gmail.com>
2014-10-21 09:12:41 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,362 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var path = require('path');
+
+  // Allow user to specify template to send through the CLI
+  var template = grunt.option('template');
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    /**
+     * Project Paths
+     */
+    paths: {
+      // Store the finished files here
+      dist: 'dist',
+
+      // Temporary folder to build things
+      tmp: 'tmp',
+
+      // Where our images are located
+      images: 'img',
+
+      // Where the email templates are located
+      templates: 'templates',
+
+      // Where the Sass files are located
+      sass: 'sass',
+
+      // Where are CSS is located
+      css: 'css',
+
+      // Where data files are living
+      data: 'data',
+
+      // Where partials are living
+      partials: 'partials',
+
+      // Where the jade files are living
+      views: 'views'
+    },
+
+    /**
+     * Before generating any new files, clean the /dist/ directory.
+     */
+    clean: {
+      dist: ['<%= paths.dist %>', '<%= paths.tmp %>']
+    },
+
+    /**
+     * Compile the Sass files using Compass
+     */
+    compass: {
+      dist: {
+        options: {
+          sassDir: '<%= paths.sass %>',
+          cssDir: '<%= paths.css %>',
+          environment: 'production'
+        }
+      }
+    },
+
+    /**
+     * Task to copy images to the dist directory
+     *
+     * This doesn't minify them, but you can run grunt dist to minify them and copy them over.
+     */
+    copy: {
+      images: {
+        expand: true,
+        cwd: '<%= paths.images %>',
+        src: ['**/*.{gif,png,jpg}'],
+        dest: '<%= paths.dist %>/img'
+      }
+    },
+
+    /**
+     * Insert the media query css into the document manually.
+     *
+     * Remember: you'll need to update each style section as you add/remove templates.
+     */
+    htmlbuild: {
+      dist: {
+        src: ['<%= paths.dist %>/*.html'],
+        dest: '<%= paths.dist %>',
+        options: {
+          relative: true,
+          styles: {
+            basic: ['<%= paths.css%>/basic.css', '<%= paths.css %>/mq.css'],
+            hero: ['<%= paths.css%>/hero.css', '<%= paths.css %>/mq.css'],
+            sidebar: ['<%= paths.css%>/sidebar.css', '<%= paths.css %>/mq.css'],
+            sidebarHero: ['<%= paths.css%>/sidebar-hero.css', '<%= paths.css %>/mq.css'],
+            actiemail: ['<%= paths.css%>/actiemail.css']
+          }
+        }
+      }
+    },
+
+    /**
+     * Image optimization
+     */
+    imagemin: {
+      options: {
+        progressive: false
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.images %>',
+          src: ['**/*.{gif,png,jpg}'],
+          dest: '<%= paths.dist %>/img'
+        }]
+      }
+    },
+
+    // Send a test email to whomever we choose
+    nodemailer: {
+
+      options: {
+
+        /**
+         * Defaults to sendmail if you don't specify a transport below
+         * See config/nodemailer-transport.json for a sample Gmail config,
+         * or visit https://github.com/andris9/Nodemailer for all options
+         */
+        transport: grunt.file.readJSON('config/nodemailer-transport.json'),
+
+        // By default, generate text from our HTML template automatically
+        generateTextFromHTML: true,
+
+        message: {
+          from: 'Ronald Konjer <ronaldkonjer@gmail.com>',
+          subject: 'Email template test'
+        },
+
+        // A collection of recipients
+        recipients: [
+         /* {
+            email: 'ronaldkonjer@gmail.com',
+            name: 'Ronald Konjer'
+          },*/
+          {
+            email: 'rkonjer@hanos.nl',
+            name: 'Ronald Konjer'
+          }
+        ]
+      },
+
+      // Load the HTML template from our dist directory.
+      dist: {
+        src: ['<%= paths.dist %>/' + template + '.html']
+      }
+    },
+
+    /**
+     * Inline the CSS from the external file into the document
+     * This also copies the templates into the /dist/ folder
+     */
+    premailer: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.tmp %>',
+          src: ['**/*.html'],
+          dest: '<%= paths.dist %>/'
+        }]
+      }
+    },
+
+    emailBuilder: {
+      inline: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.tmp %>',
+          src: ['**/*.html'],
+          dest: '<%= paths.dist %>/'
+        }]
+      }
+    },
+
+    
+    //compile the jade files
+    jade: {
+      compile: {
+        options: {
+          pretty: true,
+          data: {
+            debug: false
+          }
+        },
+        files: [{
+          expand: true, // Enable dynamic expansion.
+          cwd: '<%= paths.views %>', // Src matches are relative to this path.
+          src: ['*.jade', '!*/*.jade'],
+          dest: '<%= paths.templates %>', // Destination path prefix.
+          ext: '.html' // Dest filepaths will have this extension.
+        }]
+      }
+    },
+
+
+    /**
+     * Render EJS templates and data into HTML documents
+     */
+    render: {
+      options: {
+        data: ['<%= paths.data %>/*.json'],
+        //partialPaths: ['<%= paths.partials %>']
+      },
+
+      dev: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.templates %>',
+          src: ['**/*.html'],
+          dest: '<%= paths.dist %>'
+        }]
+      },
+
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.templates %>',
+          src: ['**/*.html'],
+          dest: '<%= paths.tmp %>'
+        }]
+      }
+    },
+
+    /**
+     * This helps us watch changes to the files and reloads/recompiles when necessary
+     */
+    watch: {
+      // Watch the .SCSS files for changes and recompile them
+      sass: {
+        files: ['<%= paths.sass %>/*.scss'],
+        tasks: ['compass','htmlbuild'],
+        options: {
+          livereload: true
+        }
+      },
+
+      // Watch the template files for changes, inline their css files again, and recompile them
+      templates: {
+        files: ['<%= paths.templates %>/*.html', '<%= paths.partials %>/**/*.html', '<%= paths.data %>/*.json'],
+        tasks: ['render:dev','htmlbuild'],
+        options: {
+          livereload: true
+        }
+      },
+
+      jade: {
+        files: ['<%= paths.views %>/**/*.jade'],
+        tasks: ['jade','htmlbuild'],
+        options: {
+          livereload: true
+        }
+      },
+
+      // Watch our files for changes and reload the browser
+      livereload: {
+        files: ['<%= paths.dist %>/*.html', '<%= paths.dist %>/img/*'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= paths.dist %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          open: true,
+          base: '<%= paths.dist %>',
+          livereload: false
+        }
+      }
+    },
+    concurrent: {
+      dev: [
+        'compass',
+        'jade',
+        'copy'
+        /*'responsive_images',*/
+        /*'copy:styles'*/
+        /*'copy:scss'*/
+      ],
+      dist: [
+        'compass',
+        'jade',
+        'imagemin'
+      ]
+    }
+
+  });
+
+	grunt.registerTask('serve', function(target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+    // In development, do everything but premailer and imagemin
+    grunt.task.run([
+      'clean',
+      'concurrent:dev',
+      'render:dev', 
+      'htmlbuild',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', function() {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve']);
+  });
+
+
+  // In development, do everything but premailer and imagemin
+  /*grunt.registerTask('dev', ['clean', 'concurrent:dev' 'render:dev', 'htmlbuild']);*/
+
+  // To distribute, do the other two steps
+  grunt.registerTask('build', [
+  	'clean', 
+  	'concurrent:dist', 
+  	'render:dist', 
+  	'emailBuilder',
+    //'premailer', 
+  	'htmlbuild'
+  ]);
+
+  // Register a send tast to simulate an email delivery
+  grunt.registerTask('send', 'Simulates an email delivery.', function() {
+    if (!template) {
+      grunt.fail.fatal('You need to specify a template to send: --template=templateName');
+    }
+
+    grunt.task.run([
+      'build',
+      'nodemailer'
+    ]);
+  });
+
+  
+  // By default, do the dev version
+  grunt.registerTask('default', ['serve']);
+}
--- README.md
+++ README.md
@@ -1,4 +1,96 @@
-email
-=====
+Responsive Email Kit
+====================
 
-sonah setalpmet liame
+A fork of Zurb's excellent [Ink responsive email template system](http://zurb.com/ink/) integrated into a Grunt and Sass/Compass workflow for automated CSS inlining, quicker editing, image minification, and email testing.
+
+## Installation
+
+Download this repository as a ZIP file or clone it to your local machine:
+
+	git clone git@github.com:happymedium/responsive-email-kit.git
+
+You'll also need the following Rubygem dependencies installed (if you don't already have them):
+
+	gem install compass
+	gem install premailer
+	gem install nokogiri
+
+You'll of course need [NodeJS](http://nodejs.org) with NPM running. We'll be using [Grunt](http://gruntjs.com) for this project, so let's install the command line client:
+
+	sudo npm install -g grunt-cli
+
+Now, head to the root of your local repository and install the Node dependencies:
+
+	npm install
+
+## Usage
+
+To compile your assets during development, run `grunt` in the command line. This will do the following:
+
+* Compile the Sass files within the `/sass/` folder and render them in the `/css/` folder.
+* Create a `/dist/` directory (or empty it if it's already there).
+* Copy all images in `/img/` to `/dist/img/`.
+* Render each of the files with corresponding EJS templates and partials, if used. The files are placed in `/dist/`.
+* Insert the `mq.css` stylesheet in the document `<head>` (so the media queries don't get stripped).
+
+Once you've compiled assets for the first time, navigate to the `/dist/` folder in your browser and select one of the HTML files to view.
+
+### Watch for Updates
+
+You can also watch for updates! Using the [LiveReload](http://livereload.com/) extension, you can run `grunt watch`. It will refresh your browser window with any changes.
+
+### Deployment
+
+When you're ready to "deploy" your changes and add them to your email marketing service, run `grunt dist` to do everything the standard command does, except:
+
+* Minify your images using `imagemin`.
+* Inline the CSS referenced in each of the HTML templates and place the finished files in the `/dist/` folder.
+
+Then, copy the HTML within the HTML template you'd like to use (find it in the `/dist/` folder) and paste it where your service directs you to.
+
+Remember, your CSS has already been inlined, so you don't need to do that step manually.
+
+You will also need to upload your images from the `/dist/img/` folder to whatever email marketing service you use.
+
+### Email Testing
+
+You can use [Nodemailer](https://github.com/andris9/Nodemailer) to send a test email with your template via the command line.
+
+1. Configure the Sender and Recipient fields within the `nodemailer` task in `Gruntfile.js`
+2. Set the values of your transport method inside `config/nodemailer-transport.json`. An example Gmail method is there right now; you can check [Nodemailer's site](https://github.com/andris9/Nodemailer#well-known-services-for-smtp) for other options.
+3. From the command line, run `grunt send --template=YOURTEMPLATENAME` (without the `.html` appended). This will run the `dist` task before sending it using Nodemailer.
+
+###  How to test with Litmus
+
+If you have a Litmus account and want to test the email in multiple clients/devices, create a new test in Litmus, copy the email address they tell you to send the email to, open up Gruntfile.js and paste it where the recipient goes. Then run grunt send --template=TEMPLATE_NAME.html to send the email to Litmus.
+
+## Editing
+
+Edit templates within the `/templates/` directory. You can add or remove them as you'd like.
+
+To add new CSS files, it is recommended to create a new Sass (`.scss`) file for your template. Grunt will automatically compile any `.scss` files to `.css` (if they don't have an underscore `_` prepended).
+
+## Changelog
+
+### 1.2.1
+
+* Update documentation to switch to the `nokogiri` gem instead of the dead `hpricot` gem (thanks to @yuvilio).
+
+### 1.2.0
+
+Fixed a few bugs and implemented email testing.
+
+* Fix error in JSON watch path
+* Update partials watch path to account for nested partials
+* Implement Nodemailer and Grunt send task (thanks to @dwightjack and his [Grunt Email Boilerplate](https://github.com/dwightjack/grunt-email-boilerplate) for inspiration)
+* Updated the `htmlbuild` task and tags to have the basic stylesheet also printed out in a `<style>` tag, allowing for usage of web fonts and other things that get stripped
+
+### 1.1.0
+
+* Implemented EJS for templating, partials, and data integration.
+* Added a `dev` and `dist` task to prevent premailer from firing every time during `watch`.
+
+### 1.0.0
+
+* Initial commit.
+* Added in Zurb's Ink templates and implemented some Sass variables for easy customization.
